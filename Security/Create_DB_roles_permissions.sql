704ea8eb05596fa9420dd443015c32a9bad289e2
mohamedabdeldayem7 <mohamed.bk10150@gmail.com>
2026-03-03 07:04:13 +0200

    edit in permissions

--- Security/Create_DB_roles_permissions.sql
+++ Security/Create_DB_roles_permissions.sql
@@ -1,21 +1,36 @@
--- Create database roles
+USE [ExamSystemDB]
+GO
 
+-- Create database roles and Grant permissions to database roles
+-- Admin: full DDL/DML -> DB Owner role
 if not exists (select 1 from sys.database_principals where name = 'db_admin')
-	create role db_admin;
+BEGIN
+    create role db_admin;
+END
+GO
+
+ALTER ROLE db_owner ADD MEMBER db_admin;
+GO 
+
 IF NOT EXISTS (SELECT 1 FROM sys.database_principals WHERE name = 'db_TrainingManager') 
+BEGIN
     CREATE ROLE db_TrainingManager;
+END
+GO
+
 IF NOT EXISTS (SELECT 1 FROM sys.database_principals WHERE name = 'db_Instructor') 
+BEGIN
     CREATE ROLE db_Instructor;
+END
+GO
+
 IF NOT EXISTS (SELECT 1 FROM sys.database_principals WHERE name = 'db_Student') 
+BEGIN
     CREATE ROLE db_Student;
+END
 GO
 
 -- Grant permissions to database roles
-
--- Admin: full DDL/DML
-GRANT CONTROL ON SCHEMA::[dbo] TO db_admin;
-
-
 -- Training manager: manage Org and Students via stored procedures
 GRANT EXECUTE ON SCHEMA::ORG TO db_TrainingManager;
 GRANT EXECUTE ON SCHEMA::USERS TO db_TrainingManager;
@@ -33,36 +48,3 @@ GRANT SELECT ON SCHEMA::ASSESSMENT TO db_Instructor;
 -- Student: limited read via views and execute allowed SPs
 GRANT SELECT ON SCHEMA::Users TO db_Student;
 GRANT EXECUTE ON SCHEMA::Assessment TO db_Student;
-
-
-USE master;
-GO
-
--- configure database for contained users (optional, but allows easier user management without needing to create logins at the server level)
-EXEC sp_configure 'show advanced options', 1;
-RECONFIGURE;
-EXEC sp_configure 'contained database authentication', 1;
-RECONFIGURE;
-GO
-
--- make sure no one else is connected to the database before changing containment settings
-ALTER DATABASE [ExamSystemDB] 
-SET SINGLE_USER 
-WITH ROLLBACK IMMEDIATE;
-GO
-
--- setting the database containment to PARTIAL to allow contained users
-ALTER DATABASE [ExamSystemDB] 
-SET CONTAINMENT = PARTIAL;
-GO
-
--- After making changes, set the database back to multi-user mode
-ALTER DATABASE [ExamSystemDB] 
-SET MULTI_USER;
-GO
-
--- Verify the containment setting
-SELECT name, containment_desc 
-FROM sys.databases 
-WHERE name = 'ExamSystemDB';
-GO
