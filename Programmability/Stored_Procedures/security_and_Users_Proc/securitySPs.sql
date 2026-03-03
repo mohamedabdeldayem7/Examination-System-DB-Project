32aac40f68a76fd6386a816050d5f20608d91dbe
mohamedabdeldayem7 <mohamed.bk10150@gmail.com>
2026-03-04 00:23:59 +0200

    add ChangePassword SP and edit iterations to be 100

--- Programmability/Stored_Procedures/security_and_Users_Proc/securitySPs.sql
+++ Programmability/Stored_Procedures/security_and_Users_Proc/securitySPs.sql
@@ -72,8 +72,8 @@ BEGIN
     BEGIN TRY
         BEGIN TRAN;
 
-            INSERT INTO Users.Account (Username, Email, PasswordHash, PasswordSalt, Role, CreatedBy)
-            VALUES (@Username, @Email, @hash, @salt, @Role, @CreatedBy);
+            INSERT INTO Users.Account (Username, Email, PasswordHash, PasswordSalt, Role, CreatedBy, PasswordIterations)
+            VALUES (@Username, @Email, @hash, @salt, @Role, @CreatedBy, 100);
 
             DECLARE @NewId INT = SCOPE_IDENTITY();
 
@@ -90,6 +90,73 @@ BEGIN
         DECLARE @ErrorMessage NVARCHAR(4000) = ERROR_MESSAGE();
         raiserror(@ErrorMessage, 16, 1);
         RAISERROR('An internal error occurred. Please contact support.', 16, 1);
+    END CATCH
+END;
+GO
+
+
+-- This procedure allows users to change their password. It verifies the old password, checks the strength of the new password, and updates both the database user password and the Users.Account table with the new hash/salt.
+CREATE OR ALTER PROCEDURE Users.usp_ChangePassword
+(
+    @OldPassword NVARCHAR(4000),
+    @NewPassword NVARCHAR(4000),
+    @Username NVARCHAR(100) = NULL
+)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    IF @Username IS NULL
+    BEGIN
+        set @Username = SUSER_NAME();
+    END
+
+    IF Users.fn_ValidatePassword(@NewPassword) = 0
+    BEGIN
+        RAISERROR('New password is too weak! Must include Upper, Lower, Number, and Special char.',16,1); RETURN;
+    END
+
+    -- Verify old password
+    DECLARE @AccountId INT, @storedHash VARBINARY(8000), @salt VARBINARY(128), @iter INT;
+    SELECT @AccountId = AccountId, @storedHash = PasswordHash, @salt = PasswordSalt, @iter = PasswordIterations
+    FROM Users.Account WHERE Username = @Username;
+
+    IF @AccountId IS NULL
+    BEGIN
+        RAISERROR('Account not found',16,1); RETURN;
+    END
+
+    DECLARE @oldAttempt VARBINARY(8000) = Users.fn_PBKDF2_SHA512_OneBlock(@OldPassword, @salt, @iter, DATALENGTH(@storedHash));
+    IF @oldAttempt <> @storedHash
+    BEGIN
+        RAISERROR('Old password incorrect',16,1); RETURN;
+    END
+
+    -- Compute new salt/hash
+    DECLARE @newSalt VARBINARY(128) = CRYPT_GEN_RANDOM(32);
+    DECLARE @newIter INT = 100;
+    DECLARE @newHash VARBINARY(8000) = Users.fn_PBKDF2_SHA512_OneBlock(@NewPassword, @newSalt, @newIter, 32);
+
+    BEGIN TRY
+        BEGIN TRAN;
+
+        -- Update DB user password (contained user)
+        DECLARE @sql NVARCHAR(MAX) = N'ALTER USER [' + REPLACE(@Username,']',']]') + N'] WITH PASSWORD = ' + QUOTENAME(@NewPassword,'''') + N';';
+        EXEC sp_executesql @sql;
+
+        -- Update account table
+        UPDATE Users.Account
+            SET PasswordHash = @newHash, PasswordSalt = @newSalt, PasswordIterations = @newIter
+        WHERE AccountId = @AccountId;
+
+        COMMIT TRAN;
+        SELECT 1 AS Success;
+    END TRY
+    BEGIN CATCH
+        IF XACT_STATE() <> 0 ROLLBACK TRAN;
+
+        DECLARE @ErrorMessage NVARCHAR(4000) = ERROR_MESSAGE();
+        RAISERROR('Password change failed: %s',16,1, @ErrorMessage);
     END CATCH
 END;
 GO
